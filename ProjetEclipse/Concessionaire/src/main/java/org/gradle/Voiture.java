92529ed339f553f1ddca6301448272237dd1b278
rochon <mickael.rochon.18@gmail.com>
2015-01-06 09:26:50 +0100

    Update Voiture.java

--- ProjetEclipse/Concessionaire/src/main/java/org/gradle/Voiture.java
+++ ProjetEclipse/Concessionaire/src/main/java/org/gradle/Voiture.java
@@ -2,4 +2,33 @@ package org.gradle;
 
 public class Voiture {
 
+	protected String modele;
+	protected int prix;
+	
+	public Voiture(String modele,int prix){
+		this.modele=modele;
+		this.prix=prix;
+	}
+	
+	public String getModele() {
+		return modele;
+	}
+
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+
+	@Override
+	public String toString() {
+		return "Voiture [modele=" + modele + ", prix=" + prix + "]";
+	}
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
 }
