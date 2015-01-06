7790d53064fc565d957387c667f5e3066576372c
yosai <youssef.saih@ymail.com>
2015-01-06 10:42:18 +0100

    ConessionaireTest [fait]

--- ProjetEclipse/Concessionaire/src/test/java/org/gradle/ConcessionaireTest.java
+++ ProjetEclipse/Concessionaire/src/test/java/org/gradle/ConcessionaireTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ConcessionaireTest {
+	
 	@Test
 	public void canConstructAConcessionaire()
 	{
@@ -10,6 +11,23 @@ public class ConcessionaireTest {
 		assertEquals("Youssef",C.getNom());
 		assertEquals(30,C.getStock().getLimite());
 		
+		
+
+		
 	}
+	@Test
+	public void canSellAndBuy()
+	{
+		Concessionnaire C = new Concessionnaire("Youssef",30);
+		Client c =new Client("SAIH","15 rue");
+		Voiture v = new Voiture("1957",50000);
+		
+		
+		assertEquals(true, C.Achat("Opel", 12000)); // Test Achat
+		
+		assertEquals(true,C.Vente(v,c)); 			// Test Vente
+	}
+	
+	
 
 }
