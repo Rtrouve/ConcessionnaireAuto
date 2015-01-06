5e52ee2a2a25927deda7210da9973a694611a44d
Rtrouve <rtrouve@hotmail.fr>
2015-01-06 09:44:05 +0100

    Update Affichage.java
    Rajouter l'affichage pour le stock.

--- ProjetEclipse/Concessionaire/src/main/java/org/gradle/Affichage.java
+++ ProjetEclipse/Concessionaire/src/main/java/org/gradle/Affichage.java
@@ -1,5 +1,24 @@
 package org.gradle;
 
 public class Affichage {
-
+	private Concessionaire c;
+	private Stock s;
+	
+	public Affichage(Concessionaire conce){
+		c=conce;
+		s=conce.getStock();
+	}
+	
+	public void affichageStock(){
+		System.out.println(c.toString());
+		System.out.println("Le stock est :");
+		if(s.getList().isEmpty()){
+			System.out.println("vide");
+		}
+		else{
+			for (Voiture v : s.getList()){
+				System.out.println("- "+v.toString());
+			}
+		}
+	}
 }
