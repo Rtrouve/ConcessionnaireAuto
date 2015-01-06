61d1f7aac8e5da632a7b6472fa53a3407d93d71d
yosai <youssef.saih@ymail.com>
2015-01-06 10:10:22 +0100

    StockTest.java

--- ProjetEclipse/Concessionaire/src/test/resources/org/gradle/StockTest.java
+++ ProjetEclipse/Concessionaire/src/test/resources/org/gradle/StockTest.java
@@ -3,5 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StockTest {
+	public void canConstructAStock()
+	{
+		Stock S = new Stock(40);
+		assertEquals(30,S.getLimite());
+	}
 
 }
